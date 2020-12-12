193c2675c7a7119fe12173717525fc866601df3d
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2020-12-12 10:37:19 +0400

    feat: add 2nd sandbox

--- prototype/README.md
+++
@@ -1,3 +0,0 @@
-# Sandbox for Javascripts
-
-##### See the deployed project [on Github pages](https://hacking-nassa-with-html.github.io/Sandbox-for-Javascript)
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //This code creates a new <p> element:
-let futurePelement = document.createElement("p");
-let results = document.createElement("p");
+let futurePelement = document.createElement('p');
+let results = document.createElement('p');
 
 //To add text to the <p> element, you must create a text node first. This code creates a text node:
-let contentOfFuturePelement = document.createTextNode("This is a Sandbox to play with Javascript.");
-let contentOfResults = document.createTextNode("See Results in the console.");
+let contentOfFuturePelement = document.createTextNode('This is a Sandbox to play with Javascript.');
+let contentOfResults = document.createTextNode('See Results in the console.');
 
 //Then you must append the text node to the < p > element:
 futurePelement.appendChild(contentOfFuturePelement);
@@ -12,9 +12,16 @@ results.appendChild(contentOfResults);
 
 //Finally you must append the new element to an existing element.
 //This code finds an existing element:
-let appendTheNewElementToAnExistingElement = document.getElementById("body");
-let appendResultsToAnExistingElement = document.getElementById("body");
+let appendTheNewElementToAnExistingElement = document.getElementById('body');
+let appendResultsToAnExistingElement = document.getElementById('body');
 
 //This code appends the new element to the existing element:
 appendTheNewElementToAnExistingElement.appendChild(futurePelement);
 appendResultsToAnExistingElement.appendChild(results);
+
+//Create chunk of HTML markup using 'afterend'
+let sendbox1 = document.getElementById('body');
+sendbox1.insertAdjacentHTML('afterend', '<p id="example1"><a href="prototype/index1.html" target="_blank">1. Works with Prototype</a></p>');
+
+let sendbox2 = document.getElementById('example1');
+sendbox2.insertAdjacentHTML('afterend', '<p id="example1"><a href="creatingElements/index2.html" target="_blank">2. The insertAdjacentHTML() method to create HTML markup</a></p>');
